065c9c800952a272bea43bab878a97836de4295c
VadymKr <symkin2007@gmail.com>
2022-12-06 14:39:40 +0200

    view

--- app/src/main/java/com/example/moviesearch/ui/favorites/FavoritesFragment.kt
+++ app/src/main/java/com/example/moviesearch/ui/favorites/FavoritesFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.moviesearch.databinding.FragmentGalleryBinding
+import com.example.moviesearch.databinding.FragmentFavoritesBinding
 
 class FavoritesFragment : Fragment() {
 
-    private var _binding: FragmentGalleryBinding? = null
+    private var _binding: FragmentFavoritesBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,10 +25,10 @@ class FavoritesFragment : Fragment() {
         val galleryViewModel =
             ViewModelProvider(this).get(FavoritesViewModel::class.java)
 
-        _binding = FragmentGalleryBinding.inflate(inflater, container, false)
+        _binding = FragmentFavoritesBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textGallery
+        val textView: TextView = binding.textFavorites
         galleryViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
--- app/src/main/java/com/example/moviesearch/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/moviesearch/ui/main/MainFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.moviesearch.databinding.FragmentHomeBinding
+import com.example.moviesearch.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
+    private var _binding: FragmentMainBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,10 +25,10 @@ class MainFragment : Fragment() {
         val homeViewModel =
             ViewModelProvider(this).get(MainViewModel::class.java)
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
+        val textView: TextView = binding.textMain
         homeViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
